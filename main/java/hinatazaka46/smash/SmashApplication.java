0fd8288b824d5acb4bcd97aba61e85d432440cf4
JunyaMasuda <49140566+JunyaMasuda@users.noreply.github.com>
2022-08-25 21:27:27 +0900

    初期設定

---
+++ main/java/hinatazaka46/smash/SmashApplication.java
@@ -0,0 +1,13 @@
+package hinatazaka46.smash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmashApplication.class, args);
+	}
+
+}
